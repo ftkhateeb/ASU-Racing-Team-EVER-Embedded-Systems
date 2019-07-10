1de50bae46376bf84f96241dbb10055a5f5b8768
aliamaim <ali.amaim@yahoo.com>
2019-07-10 04:33:31 +0200

    Used for debugging/testing in simulation (not for deployment)

---
+++ Blind Spot Awareness/lcd.h
@@ -0,0 +1,61 @@
+ /******************************************************************************
+ *
+ * Module: LCD
+ *
+ * File Name: lcd.h
+ *
+ * Description: Header file for the LCD driver
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include "common_macros.h"
+#include "micro_config.h"
+#include <stdlib.h>
+
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+/* LCD Data bits mode configuration */
+#define DATA_BITS_MODE 4
+
+/* Use higher 4 bits in the data port */
+#if (DATA_BITS_MODE == 4)
+#define UPPER_PORT_PINS
+#endif
+
+/* LCD HW Pins */
+#define RS PD0
+#define RW PD1
+#define E  PD2
+#define LCD_CTRL_PORT PORTD
+#define LCD_CTRL_PORT_DIR DDRD
+#define LCD_DATA_PORT PORTD
+#define LCD_DATA_PORT_DIR DDRD
+
+/* LCD Commands */
+#define CLEAR_COMMAND 0x01
+#define FOUR_BITS_DATA_MODE 0x02
+#define TWO_LINE_LCD_Four_BIT_MODE 0x28
+#define TWO_LINE_LCD_Eight_BIT_MODE 0x38
+#define CURSOR_OFF 0x0C
+#define CURSOR_ON 0x0E
+#define SET_CURSOR_LOCATION 0x80 
+
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+void LCD_sendCommand(uint8_t command);
+void LCD_displayCharacter(uint8_t data);
+void LCD_displayString(const char *Str);
+void LCD_init(void);
+void LCD_clearScreen(void);
+void LCD_displayStringRowColumn(uint8_t row,uint8_t col,const char *Str);
+void LCD_goToRowColumn(uint8_t row,uint8_t col);
+void LCD_intgerToString(uint32_t data);
+
+#endif /* LCD_H_ */
