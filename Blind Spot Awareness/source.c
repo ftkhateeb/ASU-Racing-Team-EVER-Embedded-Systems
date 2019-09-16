9ff473abfd824222486c990e798446c3c7aee714
ftkhateeb <52678317+ftkhateeb@users.noreply.github.com>
2019-09-17 01:19:42 +0200

    Update source.c

--- Blind Spot Awareness/source.c
+++ Blind Spot Awareness/source.c
@@ -1,24 +1,26 @@
+  
 /*
  * BlindSpotAwarness.c
  *
  * Created: 7/8/2019 10:41:08 PM
- * Author : Ali/Khateeb
+ * Author : Ali
  */ 
-#define F_CPU 1000000      
+#define F_CPU 1000000UL      
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <stdint.h>
-#include "lcd.h"
+//#include "lcd.h"
 #include <math.h>
 
-#define TIMER1_ICP1 0 
+#define TIMER1_ICP1 0
+#define DISTANCE(TIME) ((17150 * (float)TIME))  //Speed of sound * time = distance, but the sound goes and comes (so it goes the distance two times) so divide by 2
 
 enum edge {RISING, FALLING};
 
-static volatile uint16_t pulse_length_g = 0; //Length of pulse that came back from ECHO in the ultrasonic sensor.
+
 static volatile uint16_t t1_g = 0; //Time of occurrence of rising edge 
 static volatile uint16_t t2_g = 0; //Time of occurrence of falling edge
 static volatile enum edge current_edge_g = RISING;
@@ -27,39 +29,59 @@ void icu_init(void);
 
 int main(void)
 {
+	float time = 0;
+	float distance = 0;
+	uint32_t pulse_ticks_g = 0; //Number of clock ticks that passed during "high" part of the echo signal.
 	sei(); //Enable global interrupts
-    /* Replace with your application code */
-	LCD_init(); /* initialize LCD */
-	LCD_displayString("Period is = ");
+	//LCD_init(); /* initialize LCD */
+	//LCD_displayString("Distance = ");
 	icu_init();
+	
+	DDRB |= (1 << PORTB1); //Trigger pin, O/P
+	DDRB |= (1 << PORTB4);
     while (1) 
     {
-	    if(t2_g > t1_g)
-	    {
-		pulse_length_g = t2_g - t1_g;
-		LCD_goToRowColumn(1, 0);
-		LCD_intgerToString(pulse_length_g);
-    
-	    }
+		/* Sensor is triggered by 10 us high signal, give low first to ensure clean high */
+		PORTB &= ~(1 << PORTB1);
+		_delay_us(5);
+		PORTB |= (1 << PORTB1);
+		_delay_us(15);
+		PORTB &= ~(1 << PORTB1);
+		if(t2_g > t1_g)
+			pulse_ticks_g = t2_g - t1_g;
+		else
+			pulse_ticks_g = (65535 - (uint32_t)t1_g + t2_g);
+		time = (float)pulse_ticks_g * 1024 / F_CPU;
+		distance = DISTANCE(time);
+		
+		if(distance < 2500) //2.5 meters
+			PORTB |= (1 << PORTB4);
+		else
+			PORTB &= ~(1 << PORTB4);
+		
+		
+	//	LCD_goToRowColumn(1, 0);
+	//	LCD_floatToString(DISTANCE(time));
+		_delay_ms(100);
     }
 }
 
 
 /*
-* 8-bit timer
-* Init timer in Normal Mode
+* 16-bit timer
+* Init timer in Input Capture Mode
 *  
 */
 void icu_init(void)
 {
 	DDRB &= ~(1 << TIMER1_ICP1); //Set PB0 (ICP1) as I/P
 	TCCR1B = 0;
-	TCCR1B = (1 << ICES1) | (1 << CS10);
+	TCCR1B = (1 << ICES1) | (1 << CS12) | (1 << CS10);
 	/*
 	* WGM13 = 0, WGM12 = 0.  Normal Mode
 	* ICNC1 = 0. Disable Input Capture Noise Filter
 	* ICES1 = 1. Initially set the input capture at rising edge (will be toggled at each capture)
-	* CS12 CS11 CS10 (0 0 1), set clock to CPU_CLOCK.
+	* CS12 CS11 CS10 (1 0 1), set clock to CPU_CLOCK/1024.
 	*/
 	TIFR1 |= (1<<ICF1); //Clear input capture flag
 	TIMSK1 |= (1 << ICIE1); //Enable interrupts on Input Capture for timer1
